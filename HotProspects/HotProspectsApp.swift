89801d543d9452115d0c56bf8e814c49aa940e02
Juan Carlos Robledo Morales <kei_kusanagi@outlook.com>
2024-11-20 13:01:26 -0600

    Initial Commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Juan Carlos Robledo Morales on 20/11/24.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HotProspectsTests/HotProspectsTests.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsTests.swift
+//  HotProspectsTests
+//
+//  Created by Juan Carlos Robledo Morales on 20/11/24.
+//
+
+import Testing
+@testable import HotProspects
+
+struct HotProspectsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
