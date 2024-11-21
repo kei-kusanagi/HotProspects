32c04eaf19415272f2358a38ef41ed685434e9c1
Juan Carlos Robledo Morales <kei_kusanagi@outlook.com>
2024-11-21 13:45:46 -0600

    Understanding Swift’s Result type

--- HotProspects/ContentView.swift
+++ HotProspects/ContentView.swift
@@ -8,31 +8,49 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var selectedTab = "One"
+    @State private var output = ""
+    
 
     var body: some View {
-//        TabView {
-//            Text("Tab 1")
-//            Text("Tab 2")
+        Text(output)
+            .task {
+                await fetchReadings()
+            }
+    }
+
+//    func fetchReadings() async {
+//        do {
+//            let url = URL(string: "https://hws.dev/readings.json")!
+//            let (data, _) = try await URLSession.shared.data(from: url)
+//            let readings = try JSONDecoder().decode([Double].self, from: data)
+//            output = "Found \(readings.count) readings"
+//        } catch {
+//            print("Download error")
 //        }
-        TabView(selection: $selectedTab) {
-                   Button("Show Tab 2") {
-                       selectedTab = "Two"
-                   }
-                   .tabItem {
-                       Label("One", systemImage: "star")
-                   }
-                   .tag("One")
-
-                   Text("Tab 2")
-                       .tabItem {
-                           Label("Two", systemImage: "circle")
-                       }
-                       .tag("Two")
-               }
-           }
-       }
-
+//    }
+    func fetchReadings() async {
+        
+        let fetchTask = Task {
+            let url = URL(string: "https://hws.dev/readings.json")!
+            let (data, _) = try await URLSession.shared.data(from: url)
+            let readings = try JSONDecoder().decode([Double].self, from: data)
+            return "Found \(readings.count) readings"
+        }
+        
+        let result = await fetchTask.result
+//        do {
+//            output = try result.get()
+//        } catch {
+//            output = "Error: \(error.localizedDescription)"
+//        }
+        switch result {
+            case .success(let str):
+                output = str
+            case .failure(let error):
+                output = "Error: \(error.localizedDescription)"
+        }
+    }
+}
 
 #Preview {
     ContentView()
