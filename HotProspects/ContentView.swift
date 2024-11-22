c10249b0fd6a1fd5b34dc1e3004f7966827fdd6a
Juan Carlos Robledo Morales <kei_kusanagi@outlook.com>
2024-11-22 11:57:21 -0600

    Scheduling local notifications

--- HotProspects/ContentView.swift
+++ HotProspects/ContentView.swift
@@ -6,23 +6,39 @@
 //
 
 import SwiftUI
+import UserNotifications
+
 
 struct ContentView: View {
     var body: some View {
-        List {
-            Text("Taylor Swift")
-                .swipeActions {
-                    Button("Delete", systemImage: "minus.circle", role: .destructive) {
-                        print("Deleting")
+        VStack {
+            Button("Request Permission") {
+                UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
+                    if success {
+                        print("All set!")
+                    } else if let error {
+                        print(error.localizedDescription)
                     }
                 }
-                .swipeActions(edge: .leading) {
-                    Button("Pin", systemImage: "pin") {
-                        print("Pinning")
-                    }
-                    .tint(.orange)
-                }
+            }
+
+            Button("Schedule Notification") {
+                let content = UNMutableNotificationContent()
+                content.title = "Feed the cat"
+                content.subtitle = "It looks hungry"
+                content.sound = UNNotificationSound.default
+
+                // Mostrar esta notificación dentro de cinco segundos
+                let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+
+                // Escoge un identificador aleatorio
+                let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+
+                // Añade nuestra solicitud de notificación
+                UNUserNotificationCenter.current().add(request)
+            }
         }
+
     }
 }
 
