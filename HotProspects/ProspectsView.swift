a80b7ba35147eb92ea8e1e3269b1b81f763d01d8
Juan Carlos Robledo Morales <kei_kusanagi@outlook.com>
2024-11-25 17:30:21 -0600

    Building our tab bar

---
+++ HotProspects/ProspectsView.swift
@@ -0,0 +1,38 @@
+//
+//  ProspectsView.swift
+//  HotProspects
+//
+//  Created by Juan Carlos Robledo Morales on 25/11/24.
+//
+
+import SwiftUI
+
+struct ProspectsView: View {
+    enum FilterType {
+        case none, contacted, uncontacted
+    }
+    
+    let filter: FilterType
+    
+    var title: String {
+        switch filter {
+        case .none:
+            "Everyone"
+        case .contacted:
+            "Contacted people"
+        case .uncontacted:
+            "Uncontacted people"
+        }
+    }
+    
+    var body: some View {
+        NavigationStack {
+            Text("Hello, World!")
+                .navigationTitle(title)
+        }
+    }
+}
+
+#Preview {
+    ProspectsView(filter: .none)
+}
