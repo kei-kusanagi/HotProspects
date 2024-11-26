ca6a834463c7aacb14a02d5f882d57aeac126a7b
Juan Carlos Robledo Morales <kei_kusanagi@outlook.com>
2024-11-26 17:03:37 -0600

    Adding options with swipe actions

--- HotProspects/ProspectsView.swift
+++ HotProspects/ProspectsView.swift
@@ -18,6 +18,7 @@ struct ProspectsView: View {
     @Environment(\.modelContext) var modelContext
     
     @State private var isShowingScanner = false
+    @State private var selectedProspects = Set<Prospect>()
 
     
     let filter: FilterType
@@ -36,22 +37,48 @@ struct ProspectsView: View {
     }
     
     var body: some View {
-      
-        NavigationStack {
-            List(prospects) { prospect in
+         NavigationStack {
+             List(prospects, selection: $selectedProspects) { prospect in
                 VStack(alignment: .leading) {
                     Text(prospect.name)
                         .font(.headline)
                     Text(prospect.emailAddress)
                         .foregroundStyle(.secondary)
                 }
+                .swipeActions {
+                    Button("Delete", systemImage: "trash", role: .destructive) {
+                        modelContext.delete(prospect)
+                    }
+                    if prospect.isContacted {
+                        Button("Mark Uncontacted", systemImage: "person.crop.circle.badge.xmark") {
+                            prospect.isContacted.toggle()
+                        }
+                        .tint(.blue)
+                    } else {
+                        Button("Mark Contacted", systemImage: "person.crop.circle.fill.badge.checkmark") {
+                            prospect.isContacted.toggle()
+                        }
+                        .tint(.green)
+                    }
+                }
+                .tag(prospect)
             }
             .navigationTitle(title)
             .toolbar {
-                Button("Scan", systemImage: "qrcode.viewfinder") {
-                    isShowingScanner = true
-                    
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button("Scan", systemImage: "qrcode.viewfinder") {
+                        isShowingScanner = true
+                    }
                 }
+                ToolbarItem(placement: .topBarLeading) {
+                    EditButton()
+                }
+                if selectedProspects.isEmpty == false {
+                    ToolbarItem(placement: .bottomBar) {
+                        Button("Delete Selected", action: delete)
+                    }
+                }
+                
             }
             .sheet(isPresented: $isShowingScanner){
                 CodeScannerView(codeTypes: [.qr], simulatedData: "Kei Kusanagi/n kei@mail.com", completion: handleScan)
@@ -82,7 +109,13 @@ struct ProspectsView: View {
             
         case .failure(let error):
             print("Scanning failed: \(error.localizedDescription)")
-        }    }
+        }
+    }
+    func delete() {
+        for prospect in selectedProspects {
+            modelContext.delete(prospect)
+        }
+    }
 }
 
 #Preview {
