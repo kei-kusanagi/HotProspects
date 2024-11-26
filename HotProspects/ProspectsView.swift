f77346babd1f9d698a304ef6ad73bb4667537556
Juan Carlos Robledo Morales <kei_kusanagi@outlook.com>
2024-11-25 18:09:29 -0600

    Dynamically filtering our SwiftData query

--- HotProspects/ProspectsView.swift
+++ HotProspects/ProspectsView.swift
@@ -31,7 +31,14 @@ struct ProspectsView: View {
     var body: some View {
         NavigationStack {
             NavigationStack {
-                Text("People: \(prospects.count)")
+                List(prospects) { prospect in
+                    VStack(alignment: .leading) {
+                        Text(prospect.name)
+                            .font(.headline)
+                        Text(prospect.emailAddress)
+                            .foregroundStyle(.secondary)
+                    }
+                }
                     .navigationTitle(title)
                     .toolbar {
                         Button("Scan", systemImage: "qrcode.viewfinder") {
@@ -40,6 +47,17 @@ struct ProspectsView: View {
                         }
                     }
             }
+        }
+    }
+    init(filter: FilterType) {
+        self.filter = filter
+
+        if filter != .none {
+            let showContactedOnly = filter == .contacted
+
+            _prospects = Query(filter: #Predicate {
+                $0.isContacted == showContactedOnly
+            }, sort: [SortDescriptor(\Prospect.name)])
         }
     }
 }
