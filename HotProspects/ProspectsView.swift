de8c2f1024cfc00ef9a31eac54f91e65bda86e2d
Juan Carlos Robledo Morales <kei_kusanagi@outlook.com>
2024-11-26 16:41:29 -0600

    Scanning QR codes with SwiftUI

--- HotProspects/ProspectsView.swift
+++ HotProspects/ProspectsView.swift
@@ -5,6 +5,7 @@
 //  Created by Juan Carlos Robledo Morales on 25/11/24.
 //
 
+import CodeScanner
 import SwiftData
 import SwiftUI
 
@@ -16,7 +17,12 @@ struct ProspectsView: View {
     @Query(sort: \Prospect.name) var prospects: [Prospect]
     @Environment(\.modelContext) var modelContext
     
+    @State private var isShowingScanner = false
+
+    
     let filter: FilterType
+    
+    
     
     var title: String {
         switch filter {
@@ -31,22 +37,24 @@ struct ProspectsView: View {
     
     var body: some View {
       
-            NavigationStack {
-                List(prospects) { prospect in
-                    VStack(alignment: .leading) {
-                        Text(prospect.name)
-                            .font(.headline)
-                        Text(prospect.emailAddress)
-                            .foregroundStyle(.secondary)
-                    }
+        NavigationStack {
+            List(prospects) { prospect in
+                VStack(alignment: .leading) {
+                    Text(prospect.name)
+                        .font(.headline)
+                    Text(prospect.emailAddress)
+                        .foregroundStyle(.secondary)
                 }
-                    .navigationTitle(title)
-                    .toolbar {
-                        Button("Scan", systemImage: "qrcode.viewfinder") {
-                            let prospect = Prospect(name: "Paul Hudson", emailAddress: "paul@hackingwithswift.com", isContacted: false)
-                            modelContext.insert(prospect)
-                        }
-    
+            }
+            .navigationTitle(title)
+            .toolbar {
+                Button("Scan", systemImage: "qrcode.viewfinder") {
+                    isShowingScanner = true
+                    
+                }
+            }
+            .sheet(isPresented: $isShowingScanner){
+                CodeScannerView(codeTypes: [.qr], simulatedData: "Kei Kusanagi/n kei@mail.com", completion: handleScan)
             }
         }
     }
@@ -61,6 +69,20 @@ struct ProspectsView: View {
             }, sort: [SortDescriptor(\Prospect.name)])
         }
     }
+    func handleScan(result: Result<ScanResult, ScanError>){
+        isShowingScanner = false
+        
+        switch result {
+        case .success(let result):
+            let details = result.string.components(separatedBy: "/n")
+            guard details.count == 2 else { return }
+            
+            ;let person = Prospect(name: details[0], emailAddress: details[1], isContacted: false)
+            modelContext.insert(person)
+            
+        case .failure(let error):
+            print("Scanning failed: \(error.localizedDescription)")
+        }    }
 }
 
 #Preview {
